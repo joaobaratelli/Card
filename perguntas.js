2444f102b3bba9799ac8358b050b8a0f55799267
JG_Baratelli <joao.baratelli@escola.pr.gov.br>
2025-07-28 15:23:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Programação',
+    'O que é um algoritmo em programação?',
+    'Um algoritmo é uma sequência de passos lógicos para resolver um problema ou realizar uma tarefa.'
+)
+
+criaCartao(
+    'Geografia',
+    'Por que Ogaki é chamada de "Cidade da Água"?',
+    'É chamada de "Cidade da Água" por causa de sua abundância de água subterrânea pura e muitos canais.'
+)
+criaCartao(
+    'Cartografia',
+    'Para que serve um mapa?',
+    ' Serve para localizar lugares e entender a geografia.'
+)
+criaCartao(
+    'Agrimensura',
+    'O que é a agrimensura e qual sua principal finalidade?',
+    'A agrimensura é a disciplina que envolve a medição, o registro e a análise de dados espaciais da superfície terrestre. Sua principal finalidade é determinar posições relativas e absolutas de pontos, bem como a forma, o tamanho e a localização de feições naturais e artificiais na Terra. Isso é essencial para a elaboração de mapas, planejamento urbano e rural, engenharia civil, e para a garantia da segurança jurídica da propriedade.'
+)
+
